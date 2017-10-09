f4120ae8202f57f93f7392648fad3fc4b7eb0dfb
ryu2020 <ryu2020@pingry.org>
2017-10-09 01:06:03 -0400

    VECTOR

---
+++ Vector.java
@@ -0,0 +1,275 @@
+import java.util.Iterator;
+
+public class Vector<E> implements SimpleList<E>{
+
+    private Object[] arr = new Object[0];
+
+    /**
+     * Returns the number of elements in this list.  If this list contains
+     * more than <tt>Integer.MAX_VALUE</tt> elements, returns
+     * <tt>Integer.MAX_VALUE</tt>.
+     *
+     * @return the number of elements in this list
+     */
+    @Override
+    public int size() {
+        return arr.length;
+    }
+
+    /**
+     * Returns <tt>true</tt> if this list contains no elements.
+     *
+     * @return <tt>true</tt> if this list contains no elements
+     */
+    @Override
+    public boolean isEmpty() {
+        return (arr.length == 0);
+    }
+
+    /**
+     * Returns <tt>true</tt> if this list contains the specified element.
+     * More formally, returns <tt>true</tt> if and only if this list contains
+     * at least one element <tt>e</tt> such that
+     * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+     *
+     * @param o element whose presence in this list is to be tested
+     * @return <tt>true</tt> if this list contains the specified element
+     * @throws ClassCastException   if the type of the specified element
+     *                              is incompatible with this list
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException if the specified element is null and this
+     *                              list does not permit null elements
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     */
+    @Override
+    public boolean contains(Object o) {
+        for(int x = 0 ; x < arr.length; x++){
+            if(arr[x] == o){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns an iterator over the elements in this list in proper sequence.
+     *
+     * @return an iterator over the elements in this list in proper sequence
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    /**
+     * Appends the specified element to the end of this list (optional
+     * operation).
+     * <p>
+     * <p>Lists that support this operation may place limitations on what
+     * elements may be added to this list.  In particular, some
+     * lists will refuse to add null elements, and others will impose
+     * restrictions on the type of elements that may be added.  List
+     * classes should clearly specify in their documentation any restrictions
+     * on what elements may be added.
+     *
+     * @param e element to be appended to this list
+     * @return <tt>true</tt>
+     * @throws UnsupportedOperationException if the <tt>add</tt> operation
+     *                                       is not supported by this list
+     * @throws ClassCastException            if the class of the specified element
+     *                                       prevents it from being added to this list
+     * @throws NullPointerException          if the specified element is null and this
+     *                                       list does not permit null elements
+     * @throws IllegalArgumentException      if some property of this element
+     *                                       prevents it from being added to this list
+     */
+    @Override
+    public boolean add(E e) {
+        Object[] arr2 = new Object[arr.length + 1];
+        for(int x = 0; x < arr.length; x++){
+            arr2[x] = arr[x];
+        }
+        arr2[arr.length] = e;
+        return true;
+    }
+
+    /**
+     * Removes the first occurrence of the specified element from this list,
+     * if it is present (optional operation).  If this list does not contain
+     * the element, it is unchanged.  More formally, removes the element with
+     * the lowest index <tt>i</tt> such that
+     * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>
+     * (if such an element exists).  Returns <tt>true</tt> if this list
+     * contained the specified element (or equivalently, if this list changed
+     * as a result of the call).
+     *
+     * @param o element to be removed from this list, if present
+     * @return <tt>true</tt> if this list contained the specified element
+     * @throws ClassCastException            if the type of the specified element
+     *                                       is incompatible with this list
+     *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException          if the specified element is null and this
+     *                                       list does not permit null elements
+     *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws UnsupportedOperationException if the <tt>remove</tt> operation
+     *                                       is not supported by this list
+     */
+    @Override
+    public boolean remove(Object o) {
+        Object[] arr2 = new Object[arr.length - 1];
+        for(int x = 0; x < arr.length; x++){
+            if(arr[x] != o){
+                arr2[x] = arr[x];
+            }
+        }
+        arr = arr2;
+        return true;
+    }
+
+    /**
+     * Removes all of the elements from this list (optional operation).
+     * The list will be empty after this call returns.
+     */
+    @Override
+    public void clear() {
+        arr = new Object[0];
+    }
+
+    /**
+     * Returns the element at the specified position in this list.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *                                   (<tt>index &lt; 0 || index &gt;= size()</tt>)
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        return (E) arr[index];
+    }
+
+    /**
+     * Replaces the element at the specified position in this list with the
+     * specified element (optional operation).
+     *
+     * @param index   index of the element to replace
+     * @param element element to be stored at the specified position
+     * @return the element previously at the specified position
+     * @throws ClassCastException        if the class of the specified element
+     *                                   prevents it from being added to this list
+     * @throws NullPointerException      if the specified element is null and
+     *                                   this list does not permit null elements
+     * @throws IllegalArgumentException  if some property of the specified
+     *                                   element prevents it from being added to this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *                                   (<tt>index &lt; 0 || index &gt;= size()</tt>)
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public E set(int index, E element) {
+        E ret = (E) arr[index];
+        arr[index] = element;
+        return ret;
+    }
+
+    /**
+     * Inserts the specified element at the specified position in this list
+     * (optional operation).  Shifts the element currently at that position
+     * (if any) and any subsequent elements to the right (adds one to their
+     * indices).
+     *
+     * @param index   index at which the specified element is to be inserted
+     * @param element element to be inserted
+     * @throws UnsupportedOperationException if the <tt>add</tt> operation
+     *                                       is not supported by this list
+     * @throws ClassCastException            if the class of the specified element
+     *                                       prevents it from being added to this list
+     * @throws NullPointerException          if the specified element is null and
+     *                                       this list does not permit null elements
+     * @throws IllegalArgumentException      if some property of the specified
+     *                                       element prevents it from being added to this list
+     * @throws IndexOutOfBoundsException     if the index is out of range
+     *                                       (<tt>index &lt; 0 || index &gt; size()</tt>)
+     */
+    @Override
+    public void add(int index, E element) {
+        Object[] arr2 = new Object[arr.length + 1];
+        int f = 0;
+        for(int x = 0; x < arr.length; x++){
+            if(x == index){
+                f = 1;
+                arr2[x] = element;
+            }
+            arr2[x + f] = arr[x];
+        }
+        arr = arr2;
+    }
+
+    /**
+     * Removes the element at the specified position in this list (optional
+     * operation).  Shifts any subsequent elements to the left (subtracts one
+     * from their indices).  Returns the element that was removed from the
+     * list.
+     *
+     * @param index the index of the element to be removed
+     * @return the element previously at the specified position
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *                                   (<tt>index &lt; 0 || index &gt;= size()</tt>)
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public E remove(int index) {
+        E ret = null;
+        Object[] arr2 = new Object[arr.length - 1];
+        for(int x = 0; x < arr.length; x++){
+            if(x != index){
+                arr2[x] = arr[x];
+            }
+            else{
+                ret = (E) arr[x];
+            }
+        }
+        arr = arr2;
+        return ret;
+    }
+
+    /**
+     * Returns the index of the first occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     * More formally, returns the lowest index <tt>i</tt> such that
+     * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>,
+     * or -1 if there is no such index.
+     *
+     * @param o element to search for
+     * @return the index of the first occurrence of the specified element in
+     * this list, or -1 if this list does not contain the element
+     * @throws ClassCastException   if the type of the specified element
+     *                              is incompatible with this list
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException if the specified element is null and this
+     *                              list does not permit null elements
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     */
+    @Override
+    public int indexOf(Object o) {
+        for(int x = 0; x < arr.length; x++){
+            if(arr[x] == o){
+                return x;
+            }
+        }
+        return -1;
+    }
+
+    public boolean equals(Vector v){
+        if(this.size() == v.size()){
+            for(int x = 0; x < this.size(); x++){
+                if(this.get(x) != v.get(x)){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
---
+++ VectorIterator.java
@@ -0,0 +1,59 @@
+import java.util.NoSuchElementException;
+
+public class VectorIterator implements java.util.Iterator{
+
+    private Vector v;
+    private int index = 0;
+
+    VectorIterator(Vector vector){
+        v = vector;
+    }
+
+    /**
+     * Returns {@code true} if the iteration has more elements.
+     * (In other words, returns {@code true} if {@link #next} would
+     * return an element rather than throwing an exception.)
+     *
+     * @return {@code true} if the iteration has more elements
+     */
+    @Override
+    public boolean hasNext() {
+        return (index < v.size());
+    }
+
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return the next element in the iteration
+     * @throws NoSuchElementException if the iteration has no more elements
+     */
+    @Override
+    public Object next() {
+        Object ret = v.get(index);
+        index++;
+        return ret;
+    }
+
+    /**
+     * Removes from the underlying collection the last element returned
+     * by this iterator (optional operation).  This method can be called
+     * only once per call to {@link #next}.  The behavior of an iterator
+     * is unspecified if the underlying collection is modified while the
+     * iteration is in progress in any way other than by calling this
+     * method.
+     *
+     * @throws UnsupportedOperationException if the {@code remove}
+     *                                       operation is not supported by this iterator
+     * @throws IllegalStateException         if the {@code next} method has not
+     *                                       yet been called, or the {@code remove} method has already
+     *                                       been called after the last call to the {@code next}
+     *                                       method
+     * @implSpec The default implementation throws an instance of
+     * {@link UnsupportedOperationException} and performs no other action.
+     */
+    @Override
+    public void remove() {
+        v.remove(index);
+    }
+
+}
